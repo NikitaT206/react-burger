0b6fd56562581b4d76396b5a65c1d2c3182903da
NikitaT206 <acid3602@yandex.ru>
2022-05-17 02:51:31 +0300

    feat: portal

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,8 +1,33 @@
 import modalOverlayStyles from './modal-overlay.module.css'
+import { useEffect, useState } from 'react'
+import ReactDOM from "react-dom" 
+
 export default function ModalOverlay(props) {
+  const [container] = useState(() => {
+    const element = document.createElement('div')
+    element.classList.add(`${modalOverlayStyles.overlay}`)
+    return element
+  })
+
+  useEffect(() => {
+    document.body.appendChild(container)
+
+    return () => {
+      document.body.removeChild(container)
+    }
+  })
+
   return (
-    <div className={modalOverlayStyles.overlay} onClick={props.onOverlayClick}>
-      {props.children}
-    </div>
+    ReactDOM.createPortal(
+      (
+        <div className={modalOverlayStyles.overlay} onClick={props.onOverlayClick}>
+          {props.children}
+        </div>
+      ),
+      container
+    )
   )
 }
+
+
+
